110fe975af647c0c53466de34ea0559c6858d361
Arsen Martirosyan <arsenmartirosyan@protonmail.com>
2025-06-30 12:50:17 -0700

    added basic raylib setup.

--- shmup/shmup.cpp
+++ shmup/shmup.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Raylib Window");
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("testing", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();        // Close window and OpenGL context
+    return 0;
 }
 //test
